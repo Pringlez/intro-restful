8bd56c0086def2b146714446cf7effb81dae52c3
John Walsh <john.walsh16609@gmail.com>
2015-10-08 18:06:10 +0100

    Exercise Done
    1-8 exercises done, Adv 1-2 exercises done

--- rest.js
+++ rest.js
@@ -1,19 +1,35 @@
 var express = require('express');
-
+var bodyParser = require('body-parser')
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 var todos = [
-  'Clean dog.'
-  , 'Buy Groceries.'
-  , 'Send letter.'
+  'Clean dog.',
+  'Buy Groceries.',
+  'Send letter.'
 ];
 
 app.get('/', function(req, res) {
-    res.json({ message: 'It works!' });   
+    res.json({ message: 'WORKING!' });
 });
 
 app.get('/todo', function(req, res) {
-    res.json(todos);   
+    res.json(todos);
+});
+
+app.get('/todo/:id', function(req, res){
+	res.json(todos[req.params.id]);
+});
+
+app.get('/todo/delete/:id', function(req, res){
+	delete todos[req.params.id];
+	res.json(todos);
+});
+
+app.post('/todo/add/', function(req, res){
+	todos.push(req.body.addition);
+	res.json(todos);
 });
 
 var server = app.listen(8000);
